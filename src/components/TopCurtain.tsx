f24f8b37b3c5130e73a4212e30671da0cce3c4ca
OptiBots <optibots.agency@gmail.com>
2026-07-07 17:04:33 +0200

    Update TopCurtain.tsx

--- src/components/TopCurtain.tsx
+++ src/components/TopCurtain.tsx
@@ -33,8 +33,8 @@ export default function TopCurtain() {
     }
   };
 
-  return (
-    <div className="relative w-full z-50">
+ return (
+  <div className="fixed top-0 right-0 z-50">
       {/* Кнопка открытия шторки/триггер */}
       <div className="flex justify-end p-4 bg-transparent absolute right-0 top-0">
         <button 
