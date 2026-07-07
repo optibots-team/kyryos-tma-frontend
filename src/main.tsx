69fd1071b089d4707060a68ce1ec65690b6ec50d
OptiBots <optibots.agency@gmail.com>
2026-07-07 16:42:19 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import './i18n';
 import App from './App.tsx';
 import './index.css';
-import i18n from 'i18next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
